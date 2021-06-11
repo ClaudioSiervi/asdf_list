560f48ce8b4247d1f29e2eeb8b9fff5b61cfbfad
claudio.siervi <claudio.siervi@gmail.com>
2021-06-11 01:11:25 -0300

    standardizes url

--- task_list/urls.py
+++ task_list/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("users/", create_user_view, name="user-create"),
 
     path('create/', TaskCreateView.as_view(), name='task-create'),
-    path('<str:pk>/detail/', RetrieveTasklView.as_view(), name='task-detail'),
+    path('<str:pk>/retrieve/', RetrieveTasklView.as_view(), name='task-detail'),
     path('<str:pk>/', UpdateTaskView.as_view(), name='task-update'),
     path('<str:pk>/delete/', DeleteTaskView.as_view(), name='task-delete'),
     path('', ListTaskView.as_view(), name='task-list'),
