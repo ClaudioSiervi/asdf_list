fbaeda7679605191e080f86e46c128d5b1c1c3c0
claudio.siervi <claudio.siervi@gmail.com>
2021-06-15 03:13:53 -0300

    update manager

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 
-from family_list_project.menagers.users import UserManager
+from family_list_project.managers.users import UserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
