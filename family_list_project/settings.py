a2635b169db651e07a55f23eb8cf8a3eb253743a
claudio.siervi <claudio.siervi@gmail.com>
2021-07-03 22:32:06 -0300

    update STATIC_ROOT

--- family_list_project/settings.py
+++ family_list_project/settings.py
@@ -186,7 +186,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
