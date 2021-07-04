a7f8e536bd183ea43a58002b3050e59cd0d4ad29
claudio.siervi <claudio.siervi@gmail.com>
2021-07-03 22:25:18 -0300

    change STATICFILES_STORAGE

--- family_list_project/settings.py
+++ family_list_project/settings.py
@@ -197,7 +197,8 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Activate Django-Heroku
