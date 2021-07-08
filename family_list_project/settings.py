f9f157d412d4e3bee1566d93a454c425efb59cc5
claudio.siervi <claudio.siervi@gmail.com>
2021-07-08 20:16:49 -0300

    adiciona nome do autor da galeria

--- family_list_project/settings.py
+++ family_list_project/settings.py
@@ -188,7 +188,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
